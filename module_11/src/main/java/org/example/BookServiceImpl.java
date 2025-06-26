c9c87b382b870851d346a2d570d54de857affb32
ayertuyak <ayertuyak@beeline.kz>
2025-06-26 18:47:25 +0500

    update module 11

---
+++ module_11/src/main/java/org/example/BookServiceImpl.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class BookServiceImpl implements BookService{
+
+    private final BookRepository bookRepository;
+
+    @Override
+    public List<Book> listBooks() {
+        return bookRepository.getAllBooks();
+    }
+}
